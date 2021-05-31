094e6502b82f29ffd9ccc1a9ebcae2d0080ba719
kashiwaguma-hiro <kashiwaguma-hiro@dmm.com>
2021-05-31 21:22:03 +0900

    Add user id

--- design/design.go
+++ design/design.go
@@ -19,66 +19,12 @@ var _ = Service("admin", func() {
 
 	Method("users", func() {
 		Payload(func() {
-			Field(1, "first-name", String, "first-name", func() {
-				Example("fn")
-				MinLength(1)
-				MaxLength(128)
-				Format(FormatRegexp)
-				Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
-				//TODO Fromat
-			})
-			Field(2, "last-name", String, "last-name", func() {
-				Example("ln")
-				MinLength(1)
-				MaxLength(128)
-				Format(FormatRegexp)
-				Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
-				//TODO Fromat
-			})
-			Field(3, "email", String, "Email", func() {
-				Example("tom@example.com")
-				MinLength(1)
-				MaxLength(128)
-				//TODO Fromat
-			})
-			Field(4, "tel", String, "tel", func() {
-				Example("000-0000-0000")
-				MinLength(1)
-				MaxLength(128)
-				//TODO Fromat
-			})
+			users_request()
 			Required("first-name", "last-name", "email", "tel")
 		})
 
 		Result(func(){
-			Field(1, "first-name", String, "first-name", func() {
-				Example("fn")
-				MinLength(1)
-				MaxLength(128)
-				Format(FormatRegexp)
-				Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
-				//TODO Fromat
-			})
-			Field(2, "last-name", String, "last-name", func() {
-				Example("ln")
-				MinLength(1)
-				MaxLength(128)
-				Format(FormatRegexp)
-				Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
-				//TODO Fromat
-			})
-			Field(3, "email", String, "Email", func() {
-				Example("tom@example.com")
-				MinLength(1)
-				MaxLength(128)
-				//TODO Fromat
-			})
-			Field(4, "tel", String, "tel", func() {
-				Example("000-0000-0000")
-				MinLength(1)
-				MaxLength(128)
-				//TODO Fromat
-			})
+			users_response()
 		})
 
 		HTTP(func() {
@@ -89,3 +35,68 @@ var _ = Service("admin", func() {
 
 	Files("/openapi.json", "./gen/http/openapi.json")
 })
+
+func users_request(){
+	Field(1, "first-name", String, "first-name", func() {
+		Example("fn")
+		MinLength(1)
+		MaxLength(128)
+		Format(FormatRegexp)
+		Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
+		//TODO Fromat
+	})
+	Field(2, "last-name", String, "last-name", func() {
+		Example("ln")
+		MinLength(1)
+		MaxLength(128)
+		Format(FormatRegexp)
+		Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
+		//TODO Fromat
+	})
+	Field(3, "email", String, "Email", func() {
+		Example("tom@example.com")
+		MinLength(1)
+		MaxLength(128)
+		//TODO Fromat
+	})
+	Field(4, "tel", String, "tel", func() {
+		Example("000-0000-0000")
+		MinLength(1)
+		MaxLength(128)
+		//TODO Fromat
+	})
+}
+
+func users_response(){
+	Field(1, "id", Int, "id", func() {
+		Example(101)
+	})
+	Field(2, "first-name", String, "first-name", func() {
+		Example("fn")
+		MinLength(1)
+		MaxLength(128)
+		Format(FormatRegexp)
+		Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
+		//TODO Fromat
+	})
+	Field(3, "last-name", String, "last-name", func() {
+		Example("ln")
+		MinLength(1)
+		MaxLength(128)
+		Format(FormatRegexp)
+		Pattern("^[0-9a-zA-Z\\.\\-_~]*$") // URIに含まれるので、利用できる記号はRFC3986のみ
+		//TODO Fromat
+	})
+	Field(4, "email", String, "Email", func() {
+		Example("tom@example.com")
+		MinLength(1)
+		MaxLength(128)
+		//TODO Fromat
+	})
+	Field(5, "tel", String, "tel", func() {
+		Example("000-0000-0000")
+		MinLength(1)
+		MaxLength(128)
+		//TODO Fromat
+	})
+}
